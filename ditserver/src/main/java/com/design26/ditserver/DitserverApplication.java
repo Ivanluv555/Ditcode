f88b9e17cebedb6f33091a57fdb7a3c9df62934f
落灰的酒精灯 <han_fangjun@163.com>
2026-04-17 09:08:35 +0800

    建立框架2

---
+++ ditserver/src/main/java/com/design26/ditserver/DitserverApplication.java
@@ -0,0 +1,13 @@
+package com.design26.ditserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitserverApplication.class, args);
+	}
+
+}
---
+++ ditserver/src/test/java/com/design26/ditserver/DitserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design26.ditserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
